c505d2132d6665ca2075ae05d1f6e8059f553141
vinceliuice <vinceliuice@hotmail.com>
2018-09-17 17:53:47 +0800

    Fixed gnome issues and removed win tiltlebutton versions

--- parse-sass.sh
+++ parse-sass.sh
@@ -15,7 +15,7 @@ fi
 for color in "${_COLOR_VARIANTS[@]}"; do
   sassc $SASSC_OPT src/gtk-3.0/gtk${color}.{scss,css}
   echo "==> Generating the gtk${color}.css..."
-  sassc $SASSC_OPT src/gtk-3.0/gtk-win${color}.{scss,css}
+  sassc $SASSC_OPT src/gtk-3.0/gtk-gnome${color}.{scss,css}
   echo "==> Generating the gtk-win${color}.css..."
   sassc $SASSC_OPT src/gnome-shell/gnome-shell${color}.{scss,css}
   echo "==> Generating the gnome-shell${color}.css..."
