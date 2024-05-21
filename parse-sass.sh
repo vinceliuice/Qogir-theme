110c69082ba1adae809172f5725dc7ba4078095f
vinceliuice <vinceliuice@hotmail.com>
2024-05-22 00:42:45 +0800

    Fixed issues

--- parse-sass.sh
+++ parse-sass.sh
@@ -39,6 +39,8 @@ for color in "${_COLOR_D_VARIANTS[@]}"; do
   echo "==> Generating the gnome-shell-42-0${color}.css..."
   sassc $SASSC_OPT src/gnome-shell/theme-44-0/gnome-shell${color}.{scss,css}
   echo "==> Generating the gnome-shell-44-0${color}.css..."
+  sassc $SASSC_OPT src/gnome-shell/theme-46-0/gnome-shell${color}.{scss,css}
+  echo "==> Generating the gnome-shell-46-0${color}.css..."
   sassc $SASSC_OPT src/cinnamon/cinnamon${color}.{scss,css}
   echo "==> Generating the cinnamon${color}.css..."
 done
